acf2b4fd6fb0bac7b326f554d2f14820bd21407c
Gareth Redman <gareth@gareth.computer>
2021-12-29 14:50:34 +1300

    Run selection command

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,4 +1,5 @@
 nova.commands.register("shellTools.promptFilter", promptFilter);
+nova.commands.register("shellTools.runSelection", runSelection);
 
 function promptFilter(editor) {
     var command = nova.config.get('gareth.computer.ShellTools.lastCommand');
@@ -63,3 +64,53 @@ Promise.filter = (process, text) => {
         });
     });
 };
+
+Promise.execute = (process) => {
+    return new Promise((resolve, reject) => {
+        var formatted = "", error = "";
+
+        process.onStdout((line) => {
+            formatted += line;
+        });
+
+        process.onStderr((line) => {
+            console.error("stderr:", line);
+            error += line;
+        });
+
+        process.onDidExit((status) => {
+            console.log("finished with status", status);
+            if (status != 0) {
+                console.error("process finished with non-zero status");
+                reject(error);
+                return;
+            }
+            resolve(formatted);
+        });
+
+        console.log("launching process with args:", process.args);
+        process.start();
+    });
+};
+
+function runSelection(editor) {
+    var range = editor.selectedRange;
+    var command = editor.getTextInRange(range);
+    var process = new Process("/bin/sh", { args: ["-c", command] });
+    var prefix = nova.config.get("computer.gareth.ShellTools.runSelection.prefix");
+    Promise.execute(process).then((output) => {
+        var formatted = output;
+        if (formatted.endsWith("\n")) {
+            formatted = formatted.substring(0, formatted.length-1);
+        }
+        formatted = formatted.split("\n").map((line) => {
+            return prefix + line;
+        }).join("\n");
+        editor.edit((e) => {
+            e.insert(range.end, "\n" + formatted);
+        });
+    })
+    .catch((error) => {
+        nova.workspace.showErrorMessage(error);
+    });
+}
